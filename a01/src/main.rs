1282d9577bd973e643f55bd8ad41b8d56e1b1e5e
Enrique Ortuno <enrique@ortuno.net>
2024-01-27 23:00:43 +0000

    basic instructions done

--- a01/src/main.rs
+++ a01/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!"); // println! is a macro
+
+    const LUCKY_NUMBER: i32 = 7; // const: immutable
+    println!("My lucky number is {}.", LUCKY_NUMBER);
+
+    let name = "Enrique"; // let: mutable
+    println!("My name is {name}"); // {name} is a placeholder
 }
